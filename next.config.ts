ff81a5ef6aaba7fdae9902c901e5ff82cfe48881
tareksayedhassan <avakinmemo@gmail.com>
2025-10-31 21:10:41 +0200

    save

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
-// next.config.js
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
   reactStrictMode: true,
-
+  assetPrefix: "https://auto-lap.online/",
   async rewrites() {
     return [
       {
